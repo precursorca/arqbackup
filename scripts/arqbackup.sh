ee7647cf2c6eeb9dbb4386da2637f3a02d8805c9
Alex Narvey <anarvey@precursor.ca>
2022-06-26 08:23:37 -0500

    Update arqbackup.sh

--- scripts/arqbackup.sh
+++ scripts/arqbackup.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Script to collect data
 # and put the data into outputfile
@@ -10,7 +10,7 @@
 # Test for QRQbackup app exists
 if [ -d "/Applications/Arq.app" ]; then
 
-CWD=$(dirname $0)
+CWD=$(dirname "$0")
 CACHEDIR="$CWD/cache/"
 OUTPUT_FILE="${CACHEDIR}ARQbackup.txt"
 SEPARATOR=' = '
@@ -23,68 +23,54 @@ LOGLOC="/Library/Logs/ArqAgent/backup"
 elif [[ $MAJORVER = "7" ]]; then
 LOGLOC="/Library/Application Support/ArqAgent/logs/backup"
 fi
-MOST_RECENT_LOG="`ls -t "$LOGLOC" | head -n1`"
+MOST_RECENT_LOG="$(ls -t "$LOGLOC" | head -n1)"
 THELOG="${LOGLOC}/${MOST_RECENT_LOG}"
 # END Set the log location#
 
-VERSION="`cat "$THELOG" | grep version | awk '{print $6}'`"
+VERSION="$(cat "$THELOG" | grep version | awk '{print $6}')"
 
 # Create cache dir if it does not exist
 mkdir -p "${CACHEDIR}"
 
 # Get the Storage Target Location #
-DESTINATION="`cat "$THELOG" | grep location | awk '{print $7}' | tr -d '(,)' `"
+DESTINATION="$(cat "$THELOG" | grep location | awk '{print $7}' | tr -d '(,)' )"
 # END Get the Storage Target Location #
 
 # Get the Backup Source based on version number#
 if [[ $MAJORVER = "6" ]]; then
-SOURCE="`cat "$THELOG" | grep backed | awk '{$1=$2=$3=""; print $0}' | cut -d : -f 1`"
+SOURCE="$(cat "$THELOG" | grep backed | awk '{$1=$2=$3=""; print $0}' | cut -d : -f 1)"
 elif [[ $MAJORVER = "7" ]]; then
-FULLSOURCE="`cat "$THELOG" | grep backed | awk '{$1=$2=$3=""; print $0}' | cut -d : -f 1`"
+FULLSOURCE="$(cat "$THELOG" | grep backed | awk '{$1=$2=$3=""; print $0}' | cut -d : -f 1)"
 SOURCE="${FULLSOURCE##*/}"
 fi
 # END Get the Backup Source #
 
-# Get the Amount Backed Up based on version number#
-if [[ $MAJORVER = "6" ]]; then
-AMOUNT="`cat "$THELOG" | grep bytes | sed -e 's/.*:\(.*\)bytes backed up/\1/' | tr -d ','`"
-fi
-if [[ $AMOUNT -gt 1000000000000 ]]; then
-GIGABYTES=$(bc <<< "scale=3; $AMOUNT/1000000000000")
-GBAMOUNT="$GIGABYTES TB"
-elif [[ $AMOUNT -gt 1000000000 ]]; then
-GIGABYTES=$(bc <<< "scale=2; $AMOUNT/1000000000")
-GBAMOUNT="$GIGABYTES GB"
-else
-GIGABYTES=$(bc <<< "scale=2; $AMOUNT/1000000")
-GBAMOUNT="$GIGABYTES MB"
-fi
-
+# Get the Amount Backed Up
 if [[ $MAJORVER = "7" ]]; then
-GBAMOUNT="`cat "$THELOG" | grep compressed')': | awk '{print $7 $8}' | tr -d ','`"
+GBAMOUNT="$(cat "$THELOG" | grep compressed')': | awk '{print $7 $8}' | tr -d ',')"
 fi
 # END Get the Amount Backed Up#
 #echo $GBAMOUNT
 
 # Get the date of backup ending #
-SDATE="`cat "$THELOG" | grep ended | awk '{print $1}'`"
+SDATE="$(cat "$THELOG" | grep ended | awk '{print $1}')"
 # END Get the date of backup ending #
 #echo $SDATE
 
 # Get the time of backup ending #
-STIME="`cat "$THELOG" | grep ended | awk '{print $2}'`"
+STIME="$(cat "$THELOG" | grep ended | awk '{print $2}')"
 # END Get the time of backup ending #
 #echo $STIME 
 
 #Get the Unix TimeStamp of the ending #
 DATETIME="$SDATE $STIME"
 #TIMESTAMP="`date -j -f "%d-%b-%Y %H:%M:%S" "$DATETIME" +%s`"
-TIMESTAMP="`date -j -f "%d-%b-%Y %H:%M:%S" "$DATETIME" +%s`"
+TIMESTAMP="$(date -j -f "%d-%b-%Y %H:%M:%S" "$DATETIME" +%s)"
 #END Get the Unix TimeStamp of the ending #
 #echo $TIMESTAMP
 
 # Get only the first Error Explanation (-m 1)#
-REASON="`cat "$THELOG" | grep -m 1 ror: | cut -f5- -d' '`"
+REASON="$(cat "$THELOG" | grep -m 1 ror: | cut -f5- -d' ')"
 # END Get the Error Explanation #
 
 # Set the Error Status #
@@ -94,16 +80,16 @@ fi
 #END set the Error status
 
 # Output data here
-echo "version${SEPARATOR}$VERSION" > ${OUTPUT_FILE}
-echo "source${SEPARATOR}$SOURCE" >> ${OUTPUT_FILE}
-echo "destination${SEPARATOR}$DESTINATION" >> ${OUTPUT_FILE}
-echo "completed${SEPARATOR}$TIMESTAMP" >> ${OUTPUT_FILE}
-echo "amount${SEPARATOR}$GBAMOUNT" >> ${OUTPUT_FILE}
-echo "error${SEPARATOR}$REASON" >> ${OUTPUT_FILE}
-echo "status${SEPARATOR}$STATUS" >> ${OUTPUT_FILE}
+echo "version${SEPARATOR}$VERSION" > "${OUTPUT_FILE}"
+echo "source${SEPARATOR}$SOURCE" >> "${OUTPUT_FILE}"
+echo "destination${SEPARATOR}$DESTINATION" >> "${OUTPUT_FILE}"
+echo "completed${SEPARATOR}$TIMESTAMP" >> "${OUTPUT_FILE}"
+echo "amount${SEPARATOR}$GBAMOUNT" >> "${OUTPUT_FILE}"
+echo "error${SEPARATOR}$REASON" >> "${OUTPUT_FILE}"
+echo "status${SEPARATOR}$STATUS" >> "${OUTPUT_FILE}"
 #END Output data
 
 else
 # There is no App so Remove the output file
-rm -rf $OUTPUT_FILE
+rm -rf "${OUTPUT_FILE}"
 fi
