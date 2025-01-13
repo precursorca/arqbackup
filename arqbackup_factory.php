359b86daa4a9c4e7066181b86b34ed9cc9edd2d2
Alex Narvey <anarvey@precursor.ca>
2025-01-13 08:12:11 -0600

    Update arqbackup_factory.php
    Added a field for total of stored data after cleanup

--- arqbackup_factory.php
+++ arqbackup_factory.php
@@ -10,9 +10,10 @@ $factory->define(Arqbackup_model::class, function (Faker\Generator $faker) {
         'version' => $faker->word(),
         'source' => $faker->word(),
         'destination' => $faker->word(),
-        'completed' => $faker->word(),
+        'completed' => $faker->dateTimeBetween('-4 months', 'now'),
         'amount' => $faker->word(),
+        'stored' => $faker->word(),
         'error' => $faker->word(),
-        'status' => $faker->word(),
+        'status' => 'Error',
     ];
 });
